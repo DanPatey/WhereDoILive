d8ae5229333d7c6f3fdf8892dfae16ee49391e2f
Dan Patey <danpatey@danpatey.com>
2017-03-21 19:53:53 -0700

    Playing with background tasks

--- WhereDoILive/ViewController.h
+++ WhereDoILive/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *txtLatitude;
+@property (weak, nonatomic) IBOutlet UITextField *txtLongitude;
+
+@property (nonatomic, retain) CLLocationManager *locationManager;
 
 @end
-
--- WhereDoILive/ViewController.m
+++ WhereDoILive/ViewController.m
@@ -8,22 +8,57 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
+}
 
 @end
 
 @implementation ViewController
+@synthesize locationManager;
+@synthesize txtLongitude, txtLatitude;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    geocoder = [[CLGeocoder alloc] init];
+    if (locationManager == nil) {
+        locationManager = [[CLLocationManager alloc] init];
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        [locationManager requestAlwaysAuthorization];
+    }
+    [locationManager startUpdatingLocation];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *newLocation = [locations lastObject];
+    [geocoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (error == nil && [placemarks count] > 0) {
+//            NSLog(@"********* Updating Location *********");
+//            NSLog(@"latitude: %@", txtLatitude);
+//            NSLog(@"longitude: %@", txtLongitude);
+//            NSLog(@"");
+            placemark = [placemarks lastObject];
+            txtLatitude.text = [NSString stringWithFormat:@"%f",newLocation.coordinate.latitude];
+            txtLongitude.text = [NSString stringWithFormat:@"%f",newLocation.coordinate.longitude];
+            printf("locationManager Fired");
+        }
+        else {
+            NSLog(@"Cannot find location");
+        }
+    }];
 }
 
+-(void) applicationDidEnterBackground:(UIApplication *) application
+{
+    [locationManager startUpdatingLocation];
+    printf("Entered background");
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"Cannot find location");
+}
 
 @end
