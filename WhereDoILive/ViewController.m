fdbaac5cf3bc57ecac94bfe027b3d5887fd0ded3
Dan Patey <danpatey@danpatey.com>
2017-03-22 21:03:02 -0700

    Cleanup and fixing logic

--- WhereDoILive/ViewController.m
+++ WhereDoILive/ViewController.m
@@ -9,9 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController () {
-
     CLGeocoder *geocoder;
-    CLPlacemark *placemark;
 }
 
 @end
@@ -25,28 +23,50 @@
     geocoder = [[CLGeocoder alloc] init];
     if (locationManager == nil) {
         locationManager = [[CLLocationManager alloc] init];
-        locationManager.delegate = self;
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        locationManager.delegate = self;
         [locationManager requestAlwaysAuthorization];
     }
-    [locationManager startUpdatingLocation];
+    [locationManager startMonitoringSignificantLocationChanges];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    CLLocation *newLocation = [locations lastObject];
-        if (UIApplication.sharedApplication.applicationState == UIApplicationStateActive) {
-//            NSLog(@"********* Updating Location *********");
-//            NSLog(@"latitude: %@", txtLatitude);
-//            NSLog(@"longitude: %@", txtLongitude);
-//            NSLog(@"");
-            txtLatitude.text = [NSString stringWithFormat:@"%f",newLocation.coordinate.latitude];
-            txtLongitude.text = [NSString stringWithFormat:@"%f",newLocation.coordinate.longitude];
-            printf("AppIsActive\n");
-        }
-        else {
-            NSLog(@"App is backgrounded. New location is %@", locations);
-        }
+    // Pull in the latest Lat and Long
+    float latitude = manager.location.coordinate.latitude;
+    float longitude = manager.location.coordinate.longitude;
+    NSLog(@"Current Latitude is: %f", latitude);
+    NSLog(@"Current Longitude is: %f", longitude);
+    
+    // Create a comparison for 11pm
+    NSDate *earliestTime = [NSDate date];
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *earliestTimeComponents =
+    [gregorian components:(NSCalendarUnitWeekday | NSCalendarUnitWeekday) fromDate:earliestTime];
+    [earliestTimeComponents setHour:23];
+    [earliestTimeComponents setMinute:00];
+    
+    // Create a comparison for 5am
+    NSDate *latestTime = [NSDate date];
+    NSDateComponents *latestTimeComponents =
+    [gregorian components:(NSCalendarUnitWeekday | NSCalendarUnitWeekday) fromDate:latestTime];
+    [latestTimeComponents setHour:05];
+    [latestTimeComponents setMinute:00];
+
+    // Create a comparison for the current time
+    NSDate * now = [NSDate date];
+    NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+    [outputFormatter setDateFormat:@"HH:mm:ss"];
+    NSString *newDateString = [outputFormatter stringFromDate:now];
+    NSLog(@"newDateString %@", newDateString);
+    
+    // If it's between 11pm and 5am set lat and long as "home"
+    if ( [now timeIntervalSinceDate:earliestTime] > 0 && [now timeIntervalSinceDate:latestTime] < 0) {
+        printf("This phone lives at:\n latitude: %f\n longitude: %f",latitude, longitude);
+    }
+    
 }
+
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
     NSLog(@"Cannot find location");
 }
